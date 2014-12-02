7a9dceded2c6d2522c94712b1d14fb0e85d6aaee
ShaB <shab@cloud66.com>
2014-12-02 18:13:43 +0000

    traffic management needs to be required

--- lib/azure.rb
+++ lib/azure.rb
@@ -37,6 +37,7 @@ require 'azure/cloud_service_management/cloud_service_management_service'
 require 'azure/base_management/location'
 require 'azure/sql_database_management/sql_database_management_service'
 require 'azure/virtual_network_management/virtual_network_management_service'
+require 'azure/traffic_management/traffic_management_service'
 
 # add some aliases for convenience
 Azure::BlobService = Azure::Blob::BlobService
