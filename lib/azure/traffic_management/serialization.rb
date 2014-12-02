764fd56c0d5db5497c832c9f7ddcda290a74016f
ShaB <shab@cloud66.com>
2014-12-02 18:42:48 +0000

    traffic management create profile

--- lib/azure/traffic_management/serialization.rb
+++ lib/azure/traffic_management/serialization.rb
@@ -1,39 +1,38 @@
-#-------------------------------------------------------------------------
-# Copyright 2013 Microsoft Open Technologies, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#--------------------------------------------------------------------------
 require 'azure/traffic_management/profile'
 require 'base64'
 
 module Azure
-  module TrafficManagement
-    module Serialization
+	module TrafficManagement
+		module Serialization
 
-	def self.profiles_from_xml(profilesXML)
-        unless profilesXML.nil? or profilesXML.at_css('Profiles').nil?
-          profiles = profilesXML.css('Profiles Profile')
-          pfs = []
-		  profiles.each do |profile|
-            pf = Profile.new
-			pf.domain_name = xml_content(profile, 'DomainName')
-			pf.name = xml_content(profile, 'Name')
-			pf.status = xml_content(profile, 'Status')
-            pfs << pf
-          end
-          pfs
-        end
-      end
+			def self.profiles_from_xml(profilesXML)
+				unless profilesXML.nil? or profilesXML.at_css('Profiles').nil?
+					profiles = profilesXML.css('Profiles Profile')
+					pfs = []
+					profiles.each do |profile|
+						pf = Profile.new
+						pf.domain_name = xml_content(profile, 'DomainName')
+						pf.name = xml_content(profile, 'Name')
+						pf.status = xml_content(profile, 'Status')
+						pfs << pf
+					end
+					pfs
+				end
+			end
 
-    end
-  end
+			def self.profile_to_xml(profile_name, profile_dns_name)
+				builder = Nokogiri::XML::Builder.new do |xml|
+					xml.Profile(
+							'xmlns' => 'http://schemas.microsoft.com/windowsazure',
+							'xmlns:i' => 'http://www.w3.org/2001/XMLSchema-instance'
+					) do
+						xml.DomainName profile_dns_name
+						xml.Name profile_name
+					end
+				end
+				builder.doc.to_xml
+			end
+
+		end
+	end
 end
